9e8fef9fd3a33f31d19e2a02b387a0f73e96e68b
Studenikin <studenikin.v.a@icloud.com>
2022-12-05 13:36:33 +0400

    created UIButton elements

--- Navigation/TabBar/FeedViewController.swift
+++ Navigation/TabBar/FeedViewController.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+import Foundation
 
 struct Post{
     var title: String
@@ -7,13 +7,29 @@ struct Post{
 
 class FeedViewController: UIViewController{
 
-    var post = Post(title: "Hello world!")
-
-
+    private var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show eeee", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 14
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        button.frame = CGRect(x: 100, y: 400, width: 100, height: 50)
+        setupButton()
+    }
 
+    func setupButton (){
+        button.addTarget(self, action: #selector(tabButtonFeed), for: .touchUpInside)
+    }
+
+    //Прописываю поведение
+    @objc func tabButtonFeed (){
+        let postController = PostViewController()
+        navigationController?.pushViewController(postController, animated: true)
     }
 }
